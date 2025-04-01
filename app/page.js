3d9cedef6fbd9eb7bc456f8a55bcea6f79505a94
Paula Garcia <pauligarcia_05@hotmail.com>
2025-03-31 23:39:40 -0300

    start about me section

--- app/page.js
+++ app/page.js
@@ -50,8 +50,8 @@ export default function Home() {
           Github →
         </a>
       </nav>
-      <main className="h-[90vh] w-full flex items-center justify-between font-[family-name:var(--font-geist-sans)] ">
-        <div className="w-1/2">
+      <main className="h-[90vh] w-full flex flex-col items-start justify-center font-[family-name:var(--font-geist-sans)] ">
+        <section className="w-1/2">
           <h1 className="text-6xl my-2">Hello!</h1>
           <h2 className="text-3xl leading-11 my-6">
             My name is <span className="font-bold">Paula Garcia</span>, and I'm
@@ -66,9 +66,38 @@ export default function Home() {
               pauligarcia_05@hotmail.com
             </a>
           </h3>
-        </div>
-        <p className="w-1/2"></p>
+        </section>
       </main>
+        <section className="h-[90vh] w-full flex flex-col items-center justify-center">
+          <p>
+            My journey began with a passion for music, dreaming of becoming an
+            orchestral composer and clarinetist. However, life took me down a
+            different path, into the exciting world of technology and software
+            development. I discovered that programming shares a similar
+            methodology to what I used when composing music. The ability to
+            abstract and visualize the final outcome allows me to identify the
+            steps needed to achieve it. After all, music and programming are
+            languages; once you understand them, you realize they are universal
+            languages that share structure, abstraction, and logic. Both require
+            creativity and precision, iteration and continuous improvement, and,
+            above all, collaboration to achieve coherent results. I began my web
+            development career learning HTML, CSS, and JavaScript, which allowed
+            me to build dynamic and attractive interfaces. Later, I deepened my
+            knowledge in React, a powerful JavaScript library that has enabled
+            me to create more efficient and scalable web applications. I’ve used
+            various styling libraries like Bootstrap, Material UI, Tailwind, and
+            Storybook to preview components and have been involved in the
+            project lifecycle through agile methodologies. I am proactive,
+            patient, and adaptable, always focused on achieving excellent
+            results. Additionally, I have a strong ability to build solid
+            relationships with clients, thanks to my proficiency in English.
+            Currently, I am studying to become a Systems Analyst to gain a solid
+            foundation in programming. This learning will enable me to explore
+            and master various languages and skills in the technology world,
+            allowing me to continue growing professionally and face new
+            challenges with confidence.
+          </p>
+        </section>
     </div>
   );
 }
