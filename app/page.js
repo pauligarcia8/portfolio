84832c7106f18a0fb49653502d0bb9c49b389a83
Paula Garcia <pauligarcia_05@hotmail.com>
2025-07-20 11:03:39 +0200

    add scrool button

--- app/page.js
+++ app/page.js
@@ -1,14 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function Home() {
+   const [show, setShow] = useState(false);
+
+   useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //if (!show) return null; esconde toda la pagina porque el componente de la flecha esta dentro de la pagina principal, excluirla
+
   return (
     <div className="mx-auto max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-0 items-center justify-items-center min-h-screen p-5 font-[family-name:var(--font-geist-mono)]">
       <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50 flex gap-[24px] py-4 px-6 justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/#about-me"
         >
           <Image
             aria-hidden
@@ -50,11 +68,11 @@ export default function Home() {
           Github →
         </a>
       </nav>
-      <main className="h-screen w-full flex flex-col items-start justify-center font-[family-name:var(--font-geist-sans)] ">
+      <main id="main" className="h-screen w-full flex flex-col items-start justify-center font-[family-name:var(--font-geist-sans)] ">
         <section className="w-1/2">
           <h1 className="text-6xl my-2">Hello!</h1>
           <h2 className="text-3xl leading-11 my-6">
-            My name is <span className="font-bold">Paula Garcia</span>, and I'm
+            My name is <span className="font-bold">Paula Garcia</span>, and I&apos;m
             a Frontend Developer
           </h2>
           <h3 className="text-xl my-2">
@@ -68,7 +86,7 @@ export default function Home() {
           </h3>
         </section>
       </main>
-      <section className="h-screen w-full flex flex-col items-center justify-center">
+      <section id="about-me" className="h-screen w-full flex flex-col items-center justify-center">
         <p>
           My journey began with a passion for music, dreaming of becoming an
           orchestral composer and clarinetist. However, life took me down a
@@ -87,7 +105,7 @@ export default function Home() {
           which allowed me to build dynamic and attractive interfaces. Later, I
           deepened my knowledge in React, a powerful JavaScript library that has
           enabled me to create more efficient and scalable web applications.
-          I’ve used various styling libraries like Bootstrap, Material UI,
+          I have used various styling libraries like Bootstrap, Material UI,
           Tailwind, and Storybook to preview components and have been involved
           in the project lifecycle through agile methodologies. I am proactive,
           patient, and adaptable, always focused on achieving excellent results.
@@ -100,6 +118,10 @@ export default function Home() {
           confidence.
         </p>
       </section>
+      <a href="/#main" aria-label="Scroll to top" className="fixed bottom-4 right-4 z-[9999] text-black p-3 rounded-full shadow-lg hover:bg-purple-500 hover:text-white transition cursor-pointer animate-bounce">
+        <FaArrowUp  />
+
+      </a>
     </div>
   );
 }
